fe1f0239023fb1d4387b2a8b050373735bbaa289
s4mu3l2005 <samyarena2005@gmail.com>
2024-06-25 22:03:10 -0500

    loging

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("git en 4hrs ... nojdoda");
+const container = document.getElementById('container')
+const registerbtn =document.getElementById('register');
+const loginbtn = document.getElementById('login');
 
+registerbtn.addEventListener('click',()
+=> {
+    container.classList.add("active");
+});
+loginbtn.addEventListener('click',()=>{
+    container.classList.remove("active");
+})
